68b5da0b279cec7b76529c7f6d1ec66f1ffdfeed
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-25 15:59:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   return chocolateBars;
 }
 function removeElementFromEndOfArray(chocolateBars){
-  return chocolateBars.slice(1, chocolateBars.length- 1);
+  return chocolateBars.slice(0, chocolateBars.length- 1);
 }
