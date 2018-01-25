0bb76e585d58c08e9ba089776930d60a6c523e67
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-25 15:48:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var x = "hah";
 
 function addElementToBeginningOfArray(chocolateBars, x){
   var newArr = [x,...chocolateBars];
